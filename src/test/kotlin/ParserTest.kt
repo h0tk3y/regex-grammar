362ffdc76051c823d5f7a19587bbb9bef3c0d8b3
hotkey <hotkeytlt@gmail.com>
2015-05-02 21:55:51 +0300

    Better parser tests.

--- src/test/kotlin/ParserTest.kt
+++ src/test/kotlin/ParserTest.kt
@@ -6,42 +6,45 @@ import org.junit.Test as test
 import org.junit.Assert.*
 
 public class ParserTest {
-    fun testTree(expr: String, tree: String) {
+
+    fun parseAndCheck(expr: String, tree: String) {
         val result = Parser.parse(expr)
         assertEquals(expr, result.toExpression())
         assertEquals(tree, result.toString())
     }
 
     test fun simple() {
-        testTree("p", "S<T<N<p>X>>")
-        testTree("a*", "S<T<N<a>X<*>>>")
-        testTree("a|b", "S<T<N<a>X>|S<T<N<b>X>>>")
+        parseAndCheck("p", "S<T<N<p>X>>")
+        parseAndCheck("a*", "S<T<N<a>X<*>>>")
+        parseAndCheck("a|b", "S<T<N<a>X>|S<T<N<b>X>>>")
     }
 
     test fun hard() {
-        testTree("((abc*b|a)*ab(aa|b*)b)*",
+        parseAndCheck("((abc*b|a)*ab(aa|b*)b)*",
                 "S<T<N<(S<T<N<(S<T<N<a>XT<N<b>XT<N<c>X<*>T<N<b>X>>>>|S<T<N<a>X>>>)>X<*>T<N<a>XT<N<b>XT<N<(S<T<N<a>XT<N<a>X>>|S<T<N<b>X<*>>>>)>XT<N<b>X>>>>>>)>X<*>>>")
-        testTree("(a*|a*)(a*|a*)",
+        parseAndCheck("(a*|a*)(a*|a*)",
                 "S<T<N<(S<T<N<a>X<*>>|S<T<N<a>X<*>>>>)>XT<N<(S<T<N<a>X<*>>|S<T<N<a>X<*>>>>)>X>>>")
-        testTree("a|b|c|(a|b|c)|d|d*|(d*)*",
+        parseAndCheck("a|b|c|(a|b|c)|d|d*|(d*)*",
                 "S<T<N<a>X>|S<T<N<b>X>|S<T<N<c>X>|S<T<N<(S<T<N<a>X>|S<T<N<b>X>|S<T<N<c>X>>>>)>X>|S<T<N<d>X>|S<T<N<d>X<*>>|S<T<N<(S<T<N<d>X<*>>>)>X<*>>>>>>>>>")
     }
 
     test fun concat() {
-        testTree("(a)(b)",
+        parseAndCheck("(a)(b)",
                 "S<T<N<(S<T<N<a>X>>)>XT<N<(S<T<N<b>X>>)>X>>>")
-        testTree("abcdefg",
+        parseAndCheck("abcdefg",
                 "S<T<N<a>XT<N<b>XT<N<c>XT<N<d>XT<N<e>XT<N<f>XT<N<g>X>>>>>>>>")
     }
 
     test fun or() {
-        testTree("a*|b", "S<T<N<a>X<*>>|S<T<N<b>X>>>")
-        testTree("abc|def|(g*|h*)", "S<T<N<a>XT<N<b>XT<N<c>X>>>|S<T<N<d>XT<N<e>XT<N<f>X>>>|S<T<N<(S<T<N<g>X<*>>|S<T<N<h>X<*>>>>)>X>>>>")
+        parseAndCheck("a*|b", "S<T<N<a>X<*>>|S<T<N<b>X>>>")
+        parseAndCheck("abc|def|(g*|h*)", "S<T<N<a>XT<N<b>XT<N<c>X>>>|S<T<N<d>XT<N<e>XT<N<f>X>>>|S<T<N<(S<T<N<g>X<*>>|S<T<N<h>X<*>>>>)>X>>>>")
     }
 
     test fun zeroOrMore() {
-        testTree("((a*)*)*", "S<T<N<(S<T<N<(S<T<N<a>X<*>>>)>X<*>>>)>X<*>>>")
-        testTree("(a*bc|(de)*f)*", "S<T<N<(S<T<N<a>X<*>T<N<b>XT<N<c>X>>>|S<T<N<(S<T<N<d>XT<N<e>X>>>)>X<*>T<N<f>X>>>>)>X<*>>>")
+        parseAndCheck("((a*)*)*",
+                "S<T<N<(S<T<N<(S<T<N<a>X<*>>>)>X<*>>>)>X<*>>>")
+        parseAndCheck("(a*bc|(de)*f)*",
+                "S<T<N<(S<T<N<a>X<*>T<N<b>XT<N<c>X>>>|S<T<N<(S<T<N<d>XT<N<e>X>>>)>X<*>T<N<f>X>>>>)>X<*>>>")
     }
 
     test(expected = javaClass<ParserException>()) fun testFail1() {
@@ -58,5 +61,9 @@ public class ParserTest {
 
     test(expected = javaClass<ParserException>()) fun testFail4() {
         Parser.parse("x**")
+    }
+
+    test(expected = javaClass<ParserException>()) fun testFail5() {
+        Parser.parse("abc|(abc*|(x)")
     }
 }
